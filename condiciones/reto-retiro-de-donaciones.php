a5d4ddbe70ca1f6f16d903293c12caacfe7eb1c8
ChBrus <cbruno.carrillo@gmail.com>
2023-01-02 14:41:13 -0600

    Eh añadido notas al reto de la carpeta condiciones

--- condiciones/reto-retiro-de-donaciones.php
+++ condiciones/reto-retiro-de-donaciones.php
@@ -3,6 +3,11 @@
 echo "---------- Twitch Mexicano B) ----------\n";
 echo "|     Retirar dinero                    \n";
 
+/*
+    Notas
+    - El intval funciona para transformar un dato de cualquier tipo a Entero
+    - readline te regresa lo que haya escrito el usuario pero de tipo String
+*/
 $total_de_donaciones = intval(readline("| Cantidad total de donaciones: "));
 
 if (is_integer($total_de_donaciones)) {
