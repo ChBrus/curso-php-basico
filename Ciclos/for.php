fb9d4891ab36b4906c98b89542a490c025a719ab
ChBrus <cbruno.carrillo@gmail.com>
2023-01-02 16:23:21 -0600

    Ciclo for :O

---
+++ Ciclos/for.php
@@ -0,0 +1,15 @@
+<?php
+
+// OMAIGAAAAAAAAA, esto no lo sabía :O se pueden declarar 2 variables en un mismo for
+// Ciclo super loco que puede tener declarados más de 1 variable
+for ($i = 0, $j = 0; $i < 10; $i++, $j += 2) {
+    // Escribir todo nuestro código
+    echo "i = $i | j = $j" . "\n";
+}
+
+// EL ciclo que más conocemos
+for ($i=0; $i < 10; $i++) { 
+    # code...
+}
+
+echo "\n";
